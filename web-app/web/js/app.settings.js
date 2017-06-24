26347d5b988c72fef5c28cd1753c3b8575e5834b
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-06-25 00:18:58 +0530

    Add camera controls to the settings page

--- web-app/web/js/app.settings.js
+++ web-app/web/js/app.settings.js
@@ -1,3 +1,5 @@
+var dataObj = {'cmd': 'PRINT', 'data': ["any", "thing", "here"]}
+
 function backendExit() {
     paldi_post({'cmd': 'EXIT', 'data': ''}, function (data) {
         console.log(data)
@@ -8,4 +10,31 @@ function backendCrop() {
     paldi_post({'cmd': 'CROP', 'data': ''}, function (data) {
         console.log(data)
     })
+}
+
+function fetchData() {
+    console.log("======req sent======")
+    // console.log(data)
+
+    // $("#photo").prop('src', "/img/photoFromCam.jpg?" + new Date().getTime())
+
+    paldi_post(dataObj, function (data) {
+        console.log(data)
+        $('#count').text("Fetched " + data['data'][0] + " resposes from camera")
+        $("#photo").prop('src', outputPath + "?" + new Date().getTime())
+    })
+
+}
+
+function startCamera() {
+    k = $.get('http://localhost:5000/startcam')
+    $('#btn-startcam').prop("disabled", true);
+    $('#btn-stopcam').prop("disabled", true);
+    location.reload()
+}
+function stopCamera() {
+    k = $.get('http://localhost:5000/stopcam')
+    $('#btn-stopcam').prop("disabled", true);
+    $('#btn-startcam').prop("disabled", true);
+    location.reload()
 }
