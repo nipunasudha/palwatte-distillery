e372bbbe405747cfc4d3efe474bbabf16cbf6e2d
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-06-26 12:11:20 +0530

    Sketch 'add rule' backend structure

--- paldi-packages/Utilities/rule_manager.py
+++ paldi-packages/Utilities/rule_manager.py
@@ -19,5 +19,19 @@ ruleData = [
 ]
 
 
-def add_rule(ar):
-    pprint.pprint(ar)
+def add_rule(req):
+    cmd = req.form['cmd']
+    if cmd == "add":
+        i1 = req.form['i1']
+        i2 = req.form['i2']
+        i3 = req.form['i3']
+        i4 = req.form['i4']
+        i5 = req.form['i5']
+        ruleData.append({
+            "variableID": "WW",
+            "variableName": i1,
+            "minDangerValue": i2,
+            "minWarningValue": i3,
+            "maxWarningValue": i4,
+            "maxDangerValue": i5})
+    pprint.pprint(cmd)
